35fc9a1d7a94e60e494c14e566c6ec4ea339f27c
Anthony Ugwu <anthonyugwu234@gmail.com>
2021-01-19 17:19:59 +0100

    update jumga client

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <c-theme-provider>
+    <c-reset />
+    <!--Your application goes here -->
+    <Nav />
+  </c-theme-provider>
+</template>
+
+<script>
+import Nav from './components/Nav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+};
+</script>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <c-flex>
+    <c-flex>Logo</c-flex>
+    <c-flex></c-flex>
+    <c-flex></c-flex>
+  </c-flex>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
